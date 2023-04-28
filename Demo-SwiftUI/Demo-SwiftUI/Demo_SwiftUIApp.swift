faed8bcac83e2f8ff5330b70c3b107a0edf095eb
David Lin <yoxisem544@gmail.com>
2023-04-28 15:10:21 +0800

    Add demo swiftui project

---
+++ Demo-SwiftUI/Demo-SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIApp.swift
+//  Demo-SwiftUI
+//
+//  Created by David on 2023/4/28.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
